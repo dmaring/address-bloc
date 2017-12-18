1ceff2f48eeec483723112e5b304f6df5eb358a0
dmaring <andrew.maring@gmail.com>
2017-12-17 19:45:36 -1000

    completed checkpoint 4

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,7 +1,23 @@
-class AddressBook
-    attr_reader :entries
+require_relative 'entry'
 
-    def initializes
-        @entries = []
+    class AddressBook
+        attr_reader :entries
+
+        def initialize
+            @entries = []
+        end
+
+    def add_entry(name, phone_number, email)
+        # store the insertion index
+        index = 0
+        entries.each do |entry|
+            # if name comes after the entry name
+            if name < entry.name
+                break
+            end
+            index+= 1
+        end
+        # insert entry into entries using the calculated index
+        entries.insert(index, Entry.new(name, phone_number, email))
     end
 end
