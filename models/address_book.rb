cafc8fa6d42e8c065cdc16681a59868daa6561f6
dmaring <andrew.maring@gmail.com>
2017-12-27 14:32:08 -0700

    completed assignment 7

--- models/address_book.rb
+++ models/address_book.rb
@@ -53,6 +53,17 @@ class AddressBook
         end
 
         return nil
+    end
+
+    def iterative_search(name)
+
+        entries.each do |entry|
+            if name == entry.name
+                return entry
+            end
+        end
+
+        return nil
 
     end
 
